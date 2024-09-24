0c8c8fa85f6bc7885438f9001caed7efeb501b83
Honganhkts <honganhkts2610@gmail.com>
2024-09-24 22:59:10 +0700

    add lai

--- src/main/java/automation/common/CommonBase.java
+++ src/main/java/automation/common/CommonBase.java
@@ -15,13 +15,13 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CommonBase {
 	public WebDriver driver;
 	public int initWaitTime = 30;
+	public int miliSecondInitWaitTime = 5000;
 
 	public WebDriver initChromeDriver(String URL) {
 		ChromeOptions options = new ChromeOptions();
@@ -32,7 +32,7 @@ public class CommonBase {
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(initWaitTime));
 		return driver;
 	}
-	
+
 	public WebDriver initFirefoxDriver(String URL) {
 		FirefoxOptions options = new FirefoxOptions();
 		System.setProperty("webdriver.firefox.driver", System.getProperty("user.dir") + "\\driver\\geckodriver.exe");
@@ -42,6 +42,7 @@ public class CommonBase {
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(initWaitTime));
 		return driver;
 	}
+
 	public WebDriver initMSEdgeDriver(String URL) {
 		EdgeOptions options = new EdgeOptions();
 		System.setProperty("webdriver.edge.driver", System.getProperty("user.dir") + "\\driver\\msedgedriver.exe");
@@ -53,68 +54,63 @@ public class CommonBase {
 	}
 	
 	public WebDriver setupDriver(String browserName) {
-		switch(browserName.trim().toLowerCase())
-		{
-		case "chrome":
-			initChromeDriver();
+		switch (browserName.trim().toLowerCase()) {
+		case "chrome": initChromeDriver();
 			break;
-		case "firefox":
-			initFirefoxDriver();
+		case "firefox": initFirefoxDriver();
 			break;
-		case "edge":
-			initMSEdgeDriver();
+		case "edge": initMSEdgeDriver();
 			break;
-		default:
-			System.out.println("The browser name " + browserName + "is invalid, run Chrome as default option");
+		default: 
+			System.out.println("The browser name " + browserName + " is invalid, run Chrome as default option");
 			initChromeDriver();
 			break;
 		}
 		return driver;
 	}
-	public WebElement getElementPresentDOM(By locator)
-	{
+	
+	public WebElement getElementPresentDOM(By locator) {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(initWaitTime));
 		wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
 		WebElement element = driver.findElement(locator);
 		return element;
 	}
-	public List<WebElement> getListWebElement(By locator){
-		
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(initWaitTime));
-		wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
-		List<WebElement> elementList = driver.findElements(locator);
-		return elementList;
-		
-	}
 	
-	public void clickJavaScriptToElement(By locator)
-	{
-		WebElement element = getElementPresentDOM(locator); // Thay thế cho driver.findElement(locator);
+//	public List<WebElement> getListWebElement(By locator){
+//		List<WebElement> elementList = null;
+//		try {
+//			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(initWaitTime));
+//			wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+//			elementList = driver.findElements(locator);
+//			return elementList;
+//		} catch (Exception e) {
+//			return elementList;
+//		}
+//		
+//	}
+
+	public void clickJavaScript(By locator) {
+		WebElement element = getElementPresentDOM(locator);
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		try {
-		js.executeScript("arguments[0].click();", element);
-		}
-		catch(StaleElementReferenceException ex)
-		{
-			pause(3000);
-			clickJavaScriptToElement(locator);
+			js.executeScript("arguments[0].click();", element);
+		} catch (StaleElementReferenceException ex) {
+			pause(2000);
+			clickJavaScript(locator);
 		}
 	}
-	
-	public void typeJavaScriptByValue (By locator, String text)
-	{
-		WebElement element = getElementPresentDOM(locator); // Thay thế cho driver.findElement(locator);
+
+	public void typeJavaScriptByValue(By locator, String text) {
+		WebElement element = getElementPresentDOM(locator);
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		try {
-		js.executeScript("arguments[0].value= '" +text+ "'", element);
-		}
-		catch(StaleElementReferenceException ex)
-		{
+			js.executeScript("arguments[0].value = '" + text + "'", element);
+		} catch (StaleElementReferenceException e) {
 			pause(3000);
 			typeJavaScriptByValue(locator, text);
 		}
 	}
-	
+
 	public void click(By locator) {
 		WebElement element = getElementPresentDOM(locator);
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(initWaitTime));
@@ -126,48 +122,42 @@ public class CommonBase {
 		WebElement element = getElementPresentDOM(locator);
 		element.sendKeys(value);
 	}
-	
-	public void scrollToElement(By locator)
-	{
-		WebElement element = getElementPresentDOM(locator); // Thay thế cho driver.findElement(locator);
+
+	public void scrollToElement(By locator) {
+		WebElement element = getElementPresentDOM(locator);
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		try {
-		js.executeScript("arguments[0].scrollIntoView(true);", element);
-		}
-		catch(StaleElementReferenceException ex)
-		{
+			js.executeScript("arguments[0].scrollIntoView(true);", element);
+		} catch (StaleElementReferenceException e) {
 			pause(5000);
 			scrollToElement(locator);
 		}
 	}
-	
-	public void pause(int miliSecondInitWaitTime)
-	{
+
+	public void pause(int miliSecondInitWaitTime) {
 		try {
 			Thread.sleep(miliSecondInitWaitTime);
 		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	public void waitAlertPeresent()
-	{
+
+	public void waitAlert() {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(initWaitTime));
 		wait.until(ExpectedConditions.alertIsPresent());
 	}
-	
-	public boolean isElementVisibility(By locator)
-	{
+
+	public boolean isElementVisibility(By locator) {
 		try {
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(initWaitTime));
-		wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
-		return true;
-		}
-		catch(TimeoutException ex)
-		{
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(initWaitTime));
+			wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+			return true;
+		} catch (TimeoutException ex) {
 			return false;
 		}
 	}
+	
 	private WebDriver initChromeDriver() {
 		ChromeOptions options = new ChromeOptions();
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
@@ -176,7 +166,7 @@ public class CommonBase {
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(initWaitTime));
 		return driver;
 	}
-	
+
 	private WebDriver initFirefoxDriver() {
 		FirefoxOptions options = new FirefoxOptions();
 		System.setProperty("webdriver.firefox.driver", System.getProperty("user.dir") + "\\driver\\geckodriver.exe");
@@ -185,6 +175,7 @@ public class CommonBase {
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(initWaitTime));
 		return driver;
 	}
+	
 	private WebDriver initMSEdgeDriver() {
 		EdgeOptions options = new EdgeOptions();
 		System.setProperty("webdriver.edge.driver", System.getProperty("user.dir") + "\\driver\\msedgedriver.exe");
@@ -193,4 +184,6 @@ public class CommonBase {
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(initWaitTime));
 		return driver;
 	}
+	
 }
+
--- src/main/java/automation/constant/CT_PageURLs.java
+++ src/main/java/automation/constant/CT_PageURLs.java
@@ -1,23 +1,6 @@
 package automation.constant;
 
 public class CT_PageURLs {
-	public static String URL_SELENIUMEASY="https://demo.seleniumeasy.com/basic-checkbox-demo.html";
-	public static String URL_SELENIUMEASY_2="https://demo.seleniumeasy.com/basic-radiobutton-demo.html";
-	public static String URL_SELENIUMEASY_3="https://demo.seleniumeasy.com/basic-select-dropdown-demo.html";
-	public static String URL_SELENIUMEASY_4="https://demo.seleniumeasy.com/input-form-demo.html";
-	public static String URL_SELENIUMEASY_5="https://www.globalsqa.com/demo-site/select-dropdown-menu/";
-
-	public static String URL_ALADA="https://alada.vn/";
-	public static String URL_TEDU="https://tedu.com.vn/dang-nhap.html";
-	public static String URL_CODESTAR_lOGIN="http://test-system.crmstar.vn/";
-	public static String URL_GURU99="https://demo.guru99.com/test/";
-	public static String URL_AUTOMATIONTESTING="https://demo.automationtesting.in/Alerts.html";
-	public static String URL_SELENIUM_PRACTISE="http://seleniumpractise.blogspot.com/2019/01/alert-demo.html";
-	public static String URL_GURU99_2="https://demo.guru99.com/test/delete_customer.php";
-	public static String URL_GURU99_3="https://demo.guru99.com/popup.php";
-	public static String URL_CODESTAR_2="https://codestar.vn/";
-	public static String URL_MEDIAMART = "https://mediamart.vn ";
-	public static String URL_DIEMMAYHUNGANH = "https://dienmaynhapkhaugiare.com.vn";
-	public static String URL_BEPANTOAN = "https://bepantoan.vn/";
-
+	public static String URL_BEPANTOAN="https://bepantoan.vn/";
+	
 }
